d760911cc76e8b6898b864cbe77be1c3efcdd938
Estrella Ángel <estrella.angel.postigo@gmail.com>
2025-03-03 14:04:00 +0100

    Task 055: Create Entity Customers (Student 2)

---
+++ src/main/java/acme/entities/Customer.java
@@ -0,0 +1,57 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidString;
+import acme.constraints.Phone;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Customer extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
+	@Column(unique = true)
+	private String				identifier;
+
+	@Mandatory
+	@Automapped
+	@Valid
+	private Phone				phone;
+
+	@Mandatory
+	@Automapped
+	@ValidString(min = 1, max = 255)
+	private String				address;
+
+	@Mandatory
+	@Automapped
+	@ValidString(min = 1, max = 50)
+	private String				city;
+
+	@Mandatory
+	@Automapped
+	@ValidString(min = 1, max = 50)
+	private String				country;
+
+	@Optional
+	@Automapped
+	@Min(0)
+	@Max(500000)
+	private Integer				points;
+
+}
