0ded4479ecf77ca16c9fa0e33efee60df1177454
Estrella Ángel <estrella.angel.postigo@gmail.com>
2025-03-03 19:46:20 +0100

    Fix AbstractRole

--- src/main/java/acme/entities/Customer.java
+++ src/main/java/acme/entities/Customer.java
@@ -5,7 +5,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.Valid;
 
-import acme.client.components.basis.AbstractEntity;
+import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
@@ -18,7 +18,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Customer extends AbstractEntity {
+public class Customer extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
 
--- src/main/java/acme/entities/Passenger.java
+++ src/main/java/acme/entities/Passenger.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import acme.client.components.basis.AbstractEntity;
+import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
@@ -20,7 +20,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Passenger extends AbstractEntity {
+public class Passenger extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
 
