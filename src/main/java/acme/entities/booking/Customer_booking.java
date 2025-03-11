8859fc54d0de1db2c26091758d4cd33851688e32
Estrella Ángel <estrella.angel.postigo@gmail.com>
2025-03-11 17:38:47 +0100

    Fix relation between customer and booking

---
+++ src/main/java/acme/entities/booking/Customer_booking.java
@@ -0,0 +1,31 @@
+
+package acme.entities.booking;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.realms.client.Customer;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Customer_booking extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Customer			customer;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Booking				booking;
+
+}
