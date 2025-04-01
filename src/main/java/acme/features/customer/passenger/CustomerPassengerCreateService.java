8a8329b861cee8737a194facbbc609b1cd0f3910
Estrella Ángel <estrella.angel.postigo@gmail.com>
2025-04-02 00:17:50 +0200

    Create Passenger

--- src/main/java/acme/features/customer/passenger/CustomerPassengerCreateService.java
+++ src/main/java/acme/features/customer/passenger/CustomerPassengerCreateService.java
@@ -3,6 +3,7 @@ package acme.features.customer.passenger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.client.components.models.Dataset;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.passenger.Passenger;
@@ -36,6 +37,25 @@ public class CustomerPassengerCreateService extends AbstractGuiService<Customer,
 		passenger.setCustomer(customer);
 
 		super.getBuffer().addData(passenger);
+
+	}
+
+	@Override
+	public void bind(final Passenger passenger) {
+		super.bindObject(passenger, "name", "email", "passportNumber", "dateOfBirth", "specialNeeds");
+
+	}
+
+	@Override
+	public void perform(final Passenger passenger) {
+		this.repository.save(passenger);
+	}
+
+	@Override
+	public void unbind(final Passenger passenger) {
+		Dataset dataset;
+		dataset = super.unbindObject(passenger, "name", "email", "passportNumber", "dateOfBirth", "specialNeeds", "draftMode");
+		super.getResponse().addData(dataset);
 
 	}
 
