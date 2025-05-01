976abaa33a55277788e2f787fc6a9bcdf359c132
danilr16 <danilopezr9@gmail.com>
2025-05-01 19:52:40 +0200

    Created Index for Student 1 and Group Entities

--- src/main/java/acme/entities/review/Review.java
+++ src/main/java/acme/entities/review/Review.java
@@ -4,6 +4,8 @@ package acme.entities.review;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -20,6 +22,10 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "moment"), //
+	@Index(columnList = "score")
+})
 public class Review extends AbstractEntity {
 
 	//Serialisation version  -----------------------------------------
