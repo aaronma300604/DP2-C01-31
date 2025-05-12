ae938ceb0c118bde1ba12bcf5816726f32e6917d
Ivanmbus <ivamirbal@alum.us.es>
2025-05-12 17:07:06 +0200

    index added

--- src/main/java/acme/realms/employee/FlightCrewMember.java
+++ src/main/java/acme/realms/employee/FlightCrewMember.java
@@ -3,7 +3,9 @@ package acme.realms.employee;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
@@ -24,6 +26,9 @@ import lombok.Setter;
 @Setter
 @Getter
 @ValidFlightCrewMember
+@Table(indexes = {
+	@Index(columnList = "abStat")
+})
 public class FlightCrewMember extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
